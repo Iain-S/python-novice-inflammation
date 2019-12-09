528e4882dba2a526b46f356fd17ee90c897f086b
Iain <i.stenson@ic.ac.uk>
2019-12-09 11:19:13 +0000

    100% test coverage of python_check.

--- bin/python_check.py
+++ bin/python_check.py
@@ -1,28 +1,48 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 import re
-import unittest
 import sys
-from bin.util import read_markdown
+# import logging
+from util import read_markdown
 from io import StringIO
 
-# Check numpy and matplotlib are installed as they are used in the lesson
+
+# Check numpy and matplotlib are installed as they are used in the inflammation lesson
 import numpy  # noqa: F401
 import matplotlib  # noqa: F401
 
+# Check pandas is installed as it is used in the gapminder lesson
+import pandas  # noqa: F401
 
+
+# logger = logging.get_logger()
+
+
+# Todo command line args
+# Todo run all of inflammation
+# Todo print -> logging
+# Todo context manager for stringio
 class BadFormatError(Exception):
     pass
 
 
+def is_block_of_class(element, md_class_name):
+    return element.get('attr') and element['attr'].get('class') and element['attr']['class'] == md_class_name
+
+
 def is_python_block(element):
-    return element.get('attr') and element['attr'].get('class') and element['attr']['class'] == 'language-python'
+    # return element.get('attr') and element['attr'].get('class') and element['attr']['class'] == 'language-python'
+    return is_block_of_class(element, 'language-python')
 
 
 def is_output_block(element):
-    return element.get('attr') and element['attr'].get('class') and element['attr']['class'] == 'output'
+    # return element.get('attr') and element['attr'].get('class') and element['attr']['class'] == 'output'
+    return is_block_of_class(element, 'output')
 
 
 def is_error_block(element):
-    return element.get('attr') and element['attr'].get('class') and element['attr']['class'] == 'error'
+    # return element.get('attr') and element['attr'].get('class') and element['attr']['class'] == 'error'
+    return is_block_of_class(element, 'error')
 
 
 def get_code_output_and_error(element_list):
@@ -64,7 +84,9 @@ def get_code_output_and_error(element_list):
                 if code_end == -1:
                     raise BadFormatError('There should be a closing ~~~.')
                 code = code_element[code_start:code_end]
+
             except KeyError:
+                # If the element has no children
                 code = element['value']
 
             code = code[:-1]  # .rstrip(' \n')
@@ -109,9 +131,13 @@ def run_code_block(code, globals_dict=None):
 
     try:
         try:
+            # if '99th element' in code:
+            #     print(eval('element'))
             return_value = eval(code, globals_dict)
         except SyntaxError:
             # Assume that code is a statement, such as "a = 1", and execute it
+            # if '99th element' in code:
+            #     print(eval('element'))
             exec(code, globals_dict)
     except Exception as e:
         if type(e) in (IndexError, TypeError, SyntaxError, IndentationError, AssertionError, NameError):
@@ -201,9 +227,29 @@ def main():
             # '06-cond.md',
             # '07-func.md',
             # '08-errors.md',
-            '09-defensive.md',
+            # '09-defensive.md',
             # '10-debugging.md',
             # '11-cmdline.md',
+            # '01-run-quit.md',
+            # '02-variables.md',
+            # '03-types-conversion.md',
+            # '04-built-in.md',
+            # '05-coffee.md',
+            # '06-libraries.md',
+            # '07-reading-tabular.md',
+            # '08-data-frames.md',
+            # '09-plotting.md',
+            # '10-lunch.md',
+            '11-lists.md',
+            '12-for-loops.md',
+            '13-looping-data-sets.md',
+            '14-writing-functions.md',
+            '15-scope.md',
+            '16-coffee.md',
+            '17-conditionals.md',
+            '18-style.md',
+            '19-wrap.md',
+            '20-feedback.md'
             ):
 
         print("Processing", doc)
@@ -218,120 +264,5 @@ def main():
                 print('{}: {}'.format(*indent_output(key, value)))
 
 
-class TestLessonRunner(unittest.TestCase):
-    def test_returns_list(self):
-        def yield_good_elements(_):
-            yield 'some code', 'some output', 'some error'
-
-        def yield_wrong_error(_):
-            yield 'some code', 'some output', 'wrong error'
-
-        def yield_wrong_output(_):
-            yield 'some code', 'wrong output', 'some error'
-
-        def run_some_code(_, **kwargs):
-            del kwargs
-            return 'some output', 'some error'
-
-        self.assertDictEqual({'code_block': 1},
-                             run_episode((), yield_good_elements, run_some_code)[0])
-        self.assertDictEqual({'code_block': 1, 'code': 'some code', 'expected_error': 'wrong error', 'actual_error': 'some error'},
-                             run_episode((), yield_wrong_error, run_some_code)[0])
-        self.assertDictEqual({'code_block': 1, 'code': 'some code', 'expected_output': 'wrong output', 'actual_output': 'some output'},
-                             run_episode((), yield_wrong_output, run_some_code)[0])
-
-
-class TestCodeGenerator(unittest.TestCase):
-    code1 = {'attr': {'class': 'language-python'},
-             'children': [{'type': 'text',
-                          'value': 'Any Python interpreter can be used as '
-                                   'a calculator:\n~~~\n3 + 5 * 4\n~~~'}]}
-
-    output1 = {'type': 'codeblock',
-               'attr': {'class': 'output'},
-               'value': '23\n'}
-
-    error1 = {'type': 'codeblock',
-              'attr': {'class': 'error'},
-              'value': '-' * 75 + '\nIndexError                              '
-                                  '  Traceback (most recent call last)\n'
-                                  '<ipython-input-3-7974b6cdaf14> in '
-                                  '<module>()\n      3 print(word[1])\n     '
-                                  ' 4 print(word[2])\n----> 5 print(word[3])\n'
-                                  '\nIndexError: string index out of range\n',
-              'options': {'location': 73, 'ial': {'class': 'error'}}}
-
-    code2 = {'attr': {'class': 'language-python'},
-             'children': [{'type': 'text',
-                          'value': '~~~\n21 / 7\n~~~'}]}
-
-    output2 = {'type': 'codeblock',
-               'attr': {'class': 'output'},
-               'value': '3\n'}
-
-    error2 = {'type': 'codeblock',
-              'attr': {'class': 'error'},
-              'value': '-' * 75 + '\nTypeError'}
-
-    def test_generator_handles_list(self):
-        document_iterator = get_code_output_and_error([self.code1, self.output1,
-                                                       self.code2, self.output2, self.error2])
-        a, b, c = next(document_iterator)
-        self.assertEqual('3 + 5 * 4', a)
-        self.assertEqual('23', b)
-        self.assertIsNone(c)
-
-        a, b, c = next(document_iterator)
-        self.assertEqual('21 / 7', a)
-        self.assertEqual('3', b)
-        self.assertEqual('TypeError', c)
-
-    def test_raises_errors(self):
-        document_iterator = get_code_output_and_error([self.output1])
-        self.assertRaises(BadFormatError, lambda: next(document_iterator))
-
-        document_iterator = get_code_output_and_error([self.code1, self.output1, self.output1])
-        self.assertRaises(BadFormatError, lambda: next(document_iterator))
-
-
-class TestCodeExecutor(unittest.TestCase):
-    def test_expression(self):
-        self.assertTupleEqual(('23', None), run_code_block('3 + 5 * 4'))
-        self.assertTupleEqual(("'t'", None), run_code_block('"t"'))
-
-    def test_none_pass(self):
-        self.assertTupleEqual((None, None), run_code_block('None'))
-        self.assertTupleEqual((None, None), run_code_block('pass'))
-
-    def test_statement(self):
-        self.assertTupleEqual(('bananagrams', None), run_code_block('print("bananagrams")'))
-        self.assertTupleEqual(('', None), run_code_block('print("")'))
-        self.assertTupleEqual(('\t', None), run_code_block('print("\t")'))
-        self.assertTupleEqual((None, None), run_code_block('a = 1'))
-        self.assertTupleEqual(('30', None), run_code_block('a = 15\nprint(a*2)'))
-
-    def test_globals(self):
-        globals_dict = dict()
-        run_code_block('kljahoiebnjs = 20', globals_dict=globals_dict)
-        self.assertTupleEqual(('20', None), run_code_block('print(kljahoiebnjs)', globals_dict=globals_dict))
-
-    def test_errors(self):
-        self.assertTupleEqual((None, 'IndexError'), run_code_block('a = "u"[44]'))
-        self.assertTupleEqual(('j', 'TypeError'), run_code_block('print("j")\na = "b" * "c"'))
-
-        # ToDo I feel like there's a good reason this doesn't work
-        # self.assertEqual('\n', run_code('print("\\n")'))
-
-
-class TestFormatting(unittest.TestCase):
-    def test_everything_else(self):
-        self.assertTupleEqual(('    some_key', 'some_val'), indent_output('some_key', 'some_val'))
-        self.assertTupleEqual(('    some_key', 'some_val\n              other_val'), indent_output('some_key', 'some_val\nother_val'))
-
-    def test_code_block(self):
-        self.assertTupleEqual(('code_block', 'some_val'), indent_output('code_block', 'some_val'))
-
-
 if __name__ == '__main__':
-    # unittest.main()
-    main()
+    main()  # pragma: no cover
---
+++ bin/test_python_check.py
@@ -0,0 +1,171 @@
+import unittest
+import python_check
+import unittest.mock as mock
+from io import StringIO
+
+
+class TestLessonRunner(unittest.TestCase):
+    def test_returns_list(self):
+        def yield_good_elements(_):
+            yield 'some code', 'some output', 'some error'
+
+        def yield_wrong_error(_):
+            yield 'some code', 'some output', 'wrong error'
+
+        def yield_wrong_output(_):
+            yield 'some code', 'wrong output', 'some error'
+
+        def run_some_code(_, **kwargs):
+            del kwargs
+            return 'some output', 'some error'
+
+        self.assertDictEqual({'code_block': 1},
+                             python_check.run_episode((), yield_good_elements, run_some_code)[0])
+        self.assertDictEqual({'code_block': 1, 'code': 'some code', 'expected_error': 'wrong error', 'actual_error': 'some error'},
+                             python_check.run_episode((), yield_wrong_error, run_some_code)[0])
+        self.assertDictEqual({'code_block': 1, 'code': 'some code', 'expected_output': 'wrong output', 'actual_output': 'some output'},
+                             python_check.run_episode((), yield_wrong_output, run_some_code)[0])
+
+
+class TestCodeGenerator(unittest.TestCase):
+    code1 = {'attr': {'class': 'language-python'},
+             'children': [{'type': 'text',
+                           'value': 'Any Python interpreter can be used as '
+                                    'a calculator:\n~~~\n3 + 5 * 4\n~~~'}]}
+
+    output1 = {'type': 'codeblock',
+               'attr': {'class': 'output'},
+               'value': '23\n'}
+
+    error1 = {'type': 'codeblock',
+              'attr': {'class': 'error'},
+              'value': '-' * 75 + '\nIndexError                              '
+                                  '  Traceback (most recent call last)\n'
+                                  '<ipython-input-3-7974b6cdaf14> in '
+                                  '<module>()\n      3 print(word[1])\n     '
+                                  ' 4 print(word[2])\n----> 5 print(word[3])\n'
+                                  '\nIndexError: string index out of range\n',
+              'options': {'location': 73, 'ial': {'class': 'error'}}}
+
+    code2 = {'attr': {'class': 'language-python'},
+             'children': [{'type': 'text',
+                           'value': '~~~\n'},
+                          {'type': 'text',
+                           'value': 'lsquo'},
+                          {'type': 'text',
+                           'value': 'A'},
+                          {'type': 'text',
+                           'value': 'rsquo'},
+                          {'type': 'text',
+                           'value': ' * 3\n~~~'}
+                          ]}
+
+    output2 = {'type': 'codeblock',
+               'attr': {'class': 'output'},
+               'value': '\'AAA\'\n'}
+
+    error2 = {'type': 'codeblock',
+              'attr': {'class': 'error'},
+              'value': '-' * 75 + '\nTypeError'}
+
+    code3 = {'attr': {'class': 'language-python'},
+             'children': [{'type': 'text',
+                           'value': '~~~\n4 * 3'}]}
+
+    code4 = {'attr': {'class': 'language-python'},
+             'value': 'None\n'}
+
+    def test_generator_handles_list(self):
+        document_iterator = python_check.get_code_output_and_error([self.code1, self.output1,
+                                                                    self.code2, self.output2, self.error2,
+                                                                    self.code4])
+        a, b, c = next(document_iterator)
+        self.assertEqual('3 + 5 * 4', a)
+        self.assertEqual('23', b)
+        self.assertIsNone(c)
+
+        a, b, c = next(document_iterator)
+        self.assertEqual('\'A\' * 3', a)
+        self.assertEqual("'AAA'", b)
+        self.assertEqual('TypeError', c)
+
+        a, b, c = next(document_iterator)
+        self.assertEqual('None', a)
+        self.assertIsNone(b)
+        self.assertIsNone(c)
+
+    def test_raises_errors(self):
+        document_iterator = python_check.get_code_output_and_error((self.output1,))
+        self.assertRaises(python_check.BadFormatError, lambda: next(document_iterator))
+
+        document_iterator = python_check.get_code_output_and_error((self.code1, self.output1, self.output1))
+        self.assertRaises(python_check.BadFormatError, lambda: next(document_iterator))
+
+        document_iterator = python_check.get_code_output_and_error((self.code3,))
+        self.assertRaises(python_check.BadFormatError, lambda: next(document_iterator))
+
+        document_iterator = python_check.get_code_output_and_error((self.error1,))
+        self.assertRaises(python_check.BadFormatError, lambda: next(document_iterator))
+
+
+class TestCodeExecutor(unittest.TestCase):
+    def test_expression(self):
+        self.assertTupleEqual(('23', None), python_check.run_code_block('3 + 5 * 4'))
+        self.assertTupleEqual(("'t'", None), python_check.run_code_block('"t"'))
+
+    def test_none_pass(self):
+        self.assertTupleEqual((None, None), python_check.run_code_block('None'))
+        self.assertTupleEqual((None, None), python_check.run_code_block('pass'))
+
+    def test_statement(self):
+        self.assertTupleEqual(('bananagrams', None), python_check.run_code_block('print("bananagrams")'))
+        self.assertTupleEqual(('', None), python_check.run_code_block('print("")'))
+        self.assertTupleEqual(('\t', None), python_check.run_code_block('print("\t")'))
+        self.assertTupleEqual((None, None), python_check.run_code_block('a = 1'))
+        self.assertTupleEqual(('30', None), python_check.run_code_block('a = 15\nprint(a*2)'))
+
+    def test_globals(self):
+        globals_dict = dict()
+        python_check.run_code_block('kljahoiebnjs = 20', globals_dict=globals_dict)
+        self.assertTupleEqual(('20', None), python_check.run_code_block('print(kljahoiebnjs)', globals_dict=globals_dict))
+
+    def test_errors(self):
+        self.assertTupleEqual((None, 'IndexError'), python_check.run_code_block('a = "u"[44]'))
+        self.assertTupleEqual(('j', 'TypeError'), python_check.run_code_block('print("j")\na = "b" * "c"'))
+
+        with mock.patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            python_check.run_code_block('1/0')
+            self.assertEqual('Caught error: division by zero from: 1/0. Continuing anyway...\n', mock_stdout.getvalue())
+
+        # ToDo I feel like there's a good reason this doesn't work
+        # self.assertEqual('\n', run_code('print("\\n")'))
+
+
+class TestFormatting(unittest.TestCase):
+    def test_everything_else(self):
+        self.assertTupleEqual(('    some_key', 'some_val'),
+                              python_check.indent_output('some_key', 'some_val'))
+        self.assertTupleEqual(('    some_key', 'some_val\n              other_val'),
+                              python_check.indent_output('some_key', 'some_val\nother_val'))
+
+    def test_code_block(self):
+        self.assertTupleEqual(('code_block', 'some_val'), python_check.indent_output('code_block', 'some_val'))
+
+
+class TestMain(unittest.TestCase):
+    code1 = {'attr': {'class': 'language-python'},
+             'value': '1 is 1\n'}
+
+    def test_main_function(self):
+        with mock.patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            with mock.patch('python_check.read_markdown') as mock_read_markdown:
+                mock_read_markdown.return_value = {'doc': {'children': (self.code1,)}}
+                python_check.main()
+
+            # mock_read_markdown.return_value.assert_called_with('20-feedback.md')
+            self.assertEqual(10, mock_read_markdown.call_count)
+        self.assertIn('Processing 20-feedback.md', mock_stdout.getvalue())
+
+
+if __name__ == '__main__':
+    unittest.main()
